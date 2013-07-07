6306bebabb74c5178b8786fd20b197b54a12bcc5
skeevy420 <r.allen.payne@gmail.com>
2013-07-07 08:06:24 -0500

    fixes for compile

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -44,6 +44,7 @@ COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 BOARD_USES_QCOM_HARDWARE := true
+TARGET_USE_O2 := true
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
